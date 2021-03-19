5c96c4ccada4d23230ff8bba192b023acb4aeaef
ysm7663-jn <ysm7663@gmail.com>
2021-03-19 15:43:05 +0900

    2021-03-19 lsit 함수

--- Basic/DataType/section04-3.py
+++ Basic/DataType/section04-3.py
@@ -46,7 +46,7 @@ print('c - ', c)
 c[1] = ['a', 'b', 'c']
 print('c - ', c)
 # [4, ['a', 'b', 'c'], 'b', 'c', 3, 4]
-c[1:3] = []
+c[1:3] = [] # 초기화
 print('c - ', c)
 # c -  [4, 'c', 3, 4]
 del c[3]    # 삭제
@@ -55,26 +55,29 @@ print('c - ', c)
 # 리스트 함수
 a = [5, 2, 3, 1, 4]
 
-print('a - ', a)
-a.append(6)
-print('a - ', a)
-a.sort()
-print('a - ', a)
-a.reverse()
-print('a - ', a)
-print('a - ', a.index(5))
-a.insert(2, 7)
-print('a - ', a)
-a.reverse()
-a.remove(1)
-print('a - ', a)
-print('a - ', a.pop())
-print('a - ', a.pop())
-print('a - ', a)
-print('a - ', a.count(4))
+print('a - ', a)    # [5, 2, 3, 1, 4]
+a.append(6) # 끝부분에 삽입
+print('a - ', a)    # [5, 2, 3, 1, 4, 6]
+a.sort()    # 정렬 (오름차순)
+print('a - ', a)    # [1, 2, 3, 4, 5, 6]
+a.reverse() # 역정렬 (내림차순)
+print('a - ', a)    # [6, 5, 4, 3, 2, 1]
+print('a - ', a.index(5))   # 1
+a.insert(2, 7)  
+print('a - ', a)    # [6, 5, 7, 4, 3, 2, 1]
+a.reverse()         # [1, 2, 3, 4, 7, 5, 6]
+a.remove(1) # 해당 값을 제거 / del(1) : 1번 인덱스를 제거
+print('a - ', a) # [2, 3, 4, 7, 5, 6]
+print('a - ', a.pop())  # 6 [2, 3, 4, 7, 5]
+# 스택(LIFO) pop() : 맨 마지막 원소를 출력하고 제거
+print('a - ', a.pop())  # 5 [2, 3, 4, 7]
+print('a - ', a)    # [2, 3, 4, 7]
+print('a - ', a.count(4))   # 4의 개수 : 1
 ex = [8, 9]
-a.extend(ex)
-print('a - ', a)
+a.extend(ex)    #.extend() : 연장(원소만 추가가 된다.)
+print('a - ', a)    # [2, 3, 4, 7, 8, 9]
+# a.append(ex)  # [2, 3, 4, 7, [8, 9]] : 리스트 자체가 추가가 된다
+
 
 # 삭제 remove, pop, del
 
