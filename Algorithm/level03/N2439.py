eae0a8402712598ace2867e84074817bf5cc45f9
ysm7663-jn <ysm7663@gmail.com>
2021-03-23 15:04:38 +0900

    2021-03-23 N2439 별찍기-2

--- Algorithm/level03/N2439.py
+++ Algorithm/level03/N2439.py
@@ -1,8 +1,9 @@
 N = int(input())
 
 for i in range(0, N) :
-    for j in range(0, i+1) :
-        for k in range(j, N-i) :
-            print(' ', end='')
-    print('*')
+    for j in range(1, N-i) :
+        print(' ', end='')
 
+    for k in range(0, i+1) :
+        print('*', end='')
+    print()
