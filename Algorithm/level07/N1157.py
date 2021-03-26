69c48907104b29ec4d85ce994bff86aed5d52655
ysm7663-jn <ysm7663@gmail.com>
2021-03-26 14:23:40 +0900

    2021-03-26 N1157 단어 공부

---
+++ Algorithm/level07/N1157.py
@@ -0,0 +1,16 @@
+arr = input().upper()
+
+unique_arr = list(set(arr))
+
+count_arr = []
+for i in unique_arr :
+    cnt = arr.count(i)
+    count_arr.append(cnt)
+
+print(count_arr)
+
+if count_arr.count(max(count_arr)) > 1 :
+    print('?')
+else :
+    max_index = count_arr.index(max(count_arr))
+    print(unique_arr[max_index])
