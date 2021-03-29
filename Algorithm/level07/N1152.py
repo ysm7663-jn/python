6a503a63ffe5f187812094cf11f19b9864a81635
ysm7663-jn <ysm7663@gmail.com>
2021-03-29 10:31:17 +0900

    2021-03-29 N1152 단어의 개수

---
+++ Algorithm/level07/N1152.py
@@ -0,0 +1,11 @@
+str1 = list(map(str, input()))
+
+if str1[0] == ' ' and str1[len(str1)-1] == ' ' :
+    print(str1.count(' ') - 1)
+    
+elif str1[0] == ' ' or str1[len(str1)-1] == ' ' :
+    print(str1.count(' '))
+    
+else :
+    print(str1.count(' ') + 1)
+    
