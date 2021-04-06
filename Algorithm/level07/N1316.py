175fc332d64ed91ccd3d703b6cd226d13331b1e1
ysm7663-jn <ysm7663@gmail.com>
2021-04-06 13:13:47 +0900

    2021-04-06 N1316 그룹단어체커

--- Algorithm/level07/N1316.py
+++ Algorithm/level07/N1316.py
@@ -1,21 +1,15 @@
 N = int(input())
 
-# 알파벳 빈 배열
-alpha = []
-for i in range(97, 123) :
-    alpha.append(chr(i))
+answer = 0
 
 for i in range(N) :
     word = input()
-    for j in range(len(word)-1) :
-        if alpha.find(word[j]) :  # alpha 배열에 j 단어가 있을 경우
-            new_word = 
-            print('있다')
-            alpha.index(word[j]) = 0 # 해당 단어의 위치를 0으로 초기화
+    for j in range(len(word)) :
+        if j != len(word) - 1:
+            if word[j] == word[j+1] :
+                pass
+            elif word[j] in word[j+1:] :
+                break
         else :
-            print('없다')
-            break
-
-
-
-
+            answer += 1
+print(answer)
