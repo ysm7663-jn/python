3c44e6553e8054395e8afe6509f4f41cb8407ace
ysm7663-jn <ysm7663@gmail.com>
2021-04-06 14:55:57 +0900

    2021-04-06 N2292 벌집

---
+++ Algorithm/level08/N2292.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+first = 1
+plus = 6
+room = 1
+
+if N == 1 :
+    print(1)
+else :
+    while True :
+        first += plus
+        room += 1
+        if N <= first :
+            print(room)
+            break
+        plus += 6
