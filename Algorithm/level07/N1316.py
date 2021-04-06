8d047fff2f6170b2d593e9c94547cec69c3b13e7
ysm7663-jn <ysm7663@gmail.com>
2021-04-06 22:33:05 +0900

    2021-04-06 N1316 그룹단어체커

--- Algorithm/level07/N1316.py
+++ Algorithm/level07/N1316.py
@@ -5,10 +5,10 @@ answer = 0
 for i in range(N) :
     word = input()
     for j in range(len(word)) :
-        if j != len(word) - 1:
-            if word[j] == word[j+1] :
+        if j != len(word) - 1: # 마지막 단어가 아니면
+            if word[j] == word[j+1] : # 다음 단어가 같은 단어
                 pass
-            elif word[j] in word[j+1:] :
+            elif word[j] in word[j+1:] : # 그 뒤로 같은 단어가 있다면 종료
                 break
         else :
             answer += 1
