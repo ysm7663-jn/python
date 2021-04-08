f3732f632d5acbac8352fb458dbd26fe76a8db6e
ysm7663-jn <ysm7663@gmail.com>
2021-04-08 11:17:55 +0900

    2021-04-08 section07 트리

--- Algorithm_study/section07.py
+++ Algorithm_study/section07.py
@@ -55,17 +55,58 @@ class NodeMgmt :
     def search(self, value) :
         self.current_node = self.head # 루트 노드부터 시작
         while self.current_node : # 노드가 존재할때 까지
-            if self.current_node.value == value :
+            if self.current_node.value == value : # 일치하면 True 반환
                 return True
+            elif value < self.current_node.value :  # 왼쪽에 있는지
+                self.current_node = self.current_node.left
+            else : # 오른쪽에 있는지
+                self.current_node = self.current_node.right
+        return False    # 순회를 통해서 일치하는 노드가 없으면 False 반환
+    
+    # 4-4 노드 삭제
+    # 삭제를 위해 해당 노드가 있는지 확인
+    def delete(self, value) :
+        searched = False
+        self.current_node = self.head # 루트 노드 / 삭제할 노드
+        self.parent = self.head # 삭제할 노드의 부모 노드
+        while self.current_node:
+            if self.current_node.value == value : # 값을 찾으면 True 반환하고 while 문 종료
+                searched = True
+                break
             elif value < self.current_node.value :
+                self.parent = self.current_node
                 self.current_node = self.current_node.left
             else :
+                self.parent = self.current_node
                 self.current_node = self.current_node.right
-        return False
+        # 해당 노드가 트리에 없는 경우
+        if searched == False :
+            return False
 
-    # 4-4 노드 삭제
+    # Case별 코드 작성
     # 4-4-1 Leaf Node 삭제
+    # self.current_node 가 삭제할 Node, self.parent는 삭제할 Node의 Parent Node인 형태
+        if self.current_node.left == None and self.current_node.right == None : # 자식이 없는 상태
+            if value < self.parent.value :
+                self.parent.left == None
+            else :
+                self.parent.right == None
+            del self.current_node
+
     # 4-4-2 Child Node가 한 개인 Node 삭제
+        # 왼쪽에만 child_node가 있는 경우
+        if self.current_node.left != None and self.current_node.right == None :
+            if value < self.parent.value :
+                self.parent.left = self.current_node.left
+            else :
+                self.parent.right = self.current_node.left
+        # 오른쪽에만 child_node가 있는 경우
+        elif self.current_node.left == None and self.current_node.right != None :
+            if value < self.parent.value :
+                self.parent.left = self.current_node.right
+            else :
+                self.parent.right = self.current_node.right
+
     # 4-4-3 Child Node가 두 개인 Node 삭제
     """
     1. 삭제할 Node의 오른쪽 자식 중, 가장 작은 값을 삭제할 Node의 Parent Node가 가리키도록 한다
@@ -77,16 +118,32 @@ class NodeMgmt :
         - 만약 해당 Node가 오른쪽 Child Node를 가지고 있었을 경우에는, 해당 Node의 본래 Parent Node의 왼쪽 Branch가 해당 오른쪽 Child Node를 가리키게 함
     2. 삭제할 Node의 왼쪽 자식 중, 가장 큰 값을 삭제할 Node의 Parent Node가 가리키도록 한다. 
     """
-
-head = Node(1)
-BST = NodeMgmt(head)
-BST.insert(2)
-BST.insert(3)
-BST.insert(0)
-BST.insert(4)
-BST.insert(8)
-
-print(BST.search(8))
-print(BST.search(-1))
-print(BST.search(2))
-
+    if self.current_node.left != None and self.current_node.right != None : # Child Node가 두 개인 경우
+        if value < self.parent.value : # 삭제할 Node가 왼쪽에 있고, 삭제할 Node의 오른쪽 자식 중, 가장 작은 값을 가진 Node의 Child Node가 있을 때와 없을 때
+            self.change_node = self.current_node.right
+            self.change_node_parent = self.current_node.right
+            while self.change_node.left != None : # 바뀌는 노드의 왼쪽 자식(더 작은 수)의 존재 확인
+                self.change_node_parent = self.change_node # 기존 chang_node는 부모로 변경
+                self.change_node = self.change_node.left # 더 작은 수를 chang_node로 변경
+            # 대체되는 노드의 오른쪽 자식이 있을 경우
+            if self.change_node.right != None :
+                self.change_node_parent.left = self.change_node.right
+            else : 
+                self.change_node_parent.left = None
+            # 삭제된 노드로 옮겨간 chang_node에 대한 연결 정리
+            self.parent.left = self.change_node
+            self.change_node.right = self.current_node.right
+            self.change_node.left = self.current_node.left
+    else : # 삭제할 Node가 오른쪽에 있고, 삭제할 Node의 오른쪽 자식 중, 가장 작은 값을 가진 Node의 Child Node의 있을 Child Node가 때와 없을 때
+        self.change_node = self.current_node.right
+        self.change_node_parent = self.current_node.right
+        while self.change_node.left != None :
+            self.change_node_parent = self.change_node
+            self.change_node = self.change_node.left
+        if self.change_node.right != None :
+            self.change_node_parent.left = self.change_node.right
+        else :
+            self.change_node_parent.left = None
+        self.parent.right = self.change_node
+        self.change_node.left = self.current_node.left
+        self.change_node.right = self.current_node.right
