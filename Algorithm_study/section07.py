3b554287ccf3b7c7cbdf211f21bd4aedce6dc7cf
ysm7663-jn <ysm7663@gmail.com>
2021-04-08 13:58:15 +0900

    2021-04-08 section07 트리

--- Algorithm_study/section07.py
+++ Algorithm_study/section07.py
@@ -88,14 +88,13 @@ class NodeMgmt :
     # self.current_node 가 삭제할 Node, self.parent는 삭제할 Node의 Parent Node인 형태
         if self.current_node.left == None and self.current_node.right == None : # 자식이 없는 상태
             if value < self.parent.value :
-                self.parent.left == None
+                self.parent.left = None
             else :
-                self.parent.right == None
-            del self.current_node
+                self.parent.right = None
 
     # 4-4-2 Child Node가 한 개인 Node 삭제
         # 왼쪽에만 child_node가 있는 경우
-        if self.current_node.left != None and self.current_node.right == None :
+        elif self.current_node.left != None and self.current_node.right == None :
             if value < self.parent.value :
                 self.parent.left = self.current_node.left
             else :
@@ -108,42 +107,73 @@ class NodeMgmt :
                 self.parent.right = self.current_node.right
 
     # 4-4-3 Child Node가 두 개인 Node 삭제
-    """
-    1. 삭제할 Node의 오른쪽 자식 중, 가장 작은 값을 삭제할 Node의 Parent Node가 가리키도록 한다
-        - 삭제할 Node의 오른쪽 자식 선택
-        - 오른쪽 자식의 가장 왼쪽에 있는 Node를 선택
-        - 해당 Node를 삭제할 Node의 Parent Node의 왼쪽 Branch가 가리키게 함
-        - 해당 Node의 왼쪽 Branch가 삭제할 왼쪽 Child Node를 가리키게 함
-        - 해당 Node의 오른쪽 Branch가 삭제할 Node의 오른쪽 Child Node를 가리키게 함
-        - 만약 해당 Node가 오른쪽 Child Node를 가지고 있었을 경우에는, 해당 Node의 본래 Parent Node의 왼쪽 Branch가 해당 오른쪽 Child Node를 가리키게 함
-    2. 삭제할 Node의 왼쪽 자식 중, 가장 큰 값을 삭제할 Node의 Parent Node가 가리키도록 한다. 
-    """
-    if self.current_node.left != None and self.current_node.right != None : # Child Node가 두 개인 경우
-        if value < self.parent.value : # 삭제할 Node가 왼쪽에 있고, 삭제할 Node의 오른쪽 자식 중, 가장 작은 값을 가진 Node의 Child Node가 있을 때와 없을 때
-            self.change_node = self.current_node.right
-            self.change_node_parent = self.current_node.right
-            while self.change_node.left != None : # 바뀌는 노드의 왼쪽 자식(더 작은 수)의 존재 확인
-                self.change_node_parent = self.change_node # 기존 chang_node는 부모로 변경
-                self.change_node = self.change_node.left # 더 작은 수를 chang_node로 변경
-            # 대체되는 노드의 오른쪽 자식이 있을 경우
-            if self.change_node.right != None :
-                self.change_node_parent.left = self.change_node.right
-            else : 
-                self.change_node_parent.left = None
-            # 삭제된 노드로 옮겨간 chang_node에 대한 연결 정리
-            self.parent.left = self.change_node
-            self.change_node.right = self.current_node.right
-            self.change_node.left = self.current_node.left
-    else : # 삭제할 Node가 오른쪽에 있고, 삭제할 Node의 오른쪽 자식 중, 가장 작은 값을 가진 Node의 Child Node의 있을 Child Node가 때와 없을 때
-        self.change_node = self.current_node.right
-        self.change_node_parent = self.current_node.right
-        while self.change_node.left != None :
-            self.change_node_parent = self.change_node
-            self.change_node = self.change_node.left
-        if self.change_node.right != None :
-            self.change_node_parent.left = self.change_node.right
-        else :
-            self.change_node_parent.left = None
-        self.parent.right = self.change_node
-        self.change_node.left = self.current_node.left
-        self.change_node.right = self.current_node.right
+        elif self.current_node.left != None and self.current_node.right != None: # Child Node가 두 개인 경우
+            if value < self.parent.value : # 삭제할 Node가 왼쪽에 있고, 삭제할 Node의 오른쪽 자식 중, 가장 작은 값을 가진 Node의 Child Node가 있을 때와 없을 때
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None : # 바뀌는 노드의 왼쪽 자식(더 작은 수)의 존재 확인
+                    self.change_node_parent = self.change_node # 기존 chang_node는 부모로 변경
+                    self.change_node = self.change_node.left # 더 작은 수를 chang_node로 변경
+                # 대체되는 노드의 오른쪽 자식이 있을 경우
+                if self.change_node.right != None :
+                    self.change_node_parent.left = self.change_node.right
+                else :
+                    self.change_node_parent.left = None
+                # 삭제된 노드로 옮겨간 chang_node에 대한 연결 정리
+                self.parent.left = self.change_node
+                self.change_node.right = self.current_node.right
+                self.change_node.left = self.current_node.left
+            else : # 삭제할 Node가 오른쪽에 있고, 삭제할 Node의 오른쪽 자식 중, 가장 작은 값을 가진 Node의 Child Node의 있을 Child Node가 때와 없을 때
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None :
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None :
+                    self.change_node_parent.left = self.change_node.right
+                else :
+                    self.change_node_parent.left = None
+                self.parent.right = self.change_node
+                self.change_node.left = self.current_node.left
+                self.change_node.right = self.current_node.right
+
+# 1 ~ 999 숫자 중에서 임의로 100개를 추출해서, 이진 탐색 트리에 입력, 검색, 삭제
+# random 라이브러리 : random.randint(0, 999) : 0에서 999까지 숫자중 특정 숫자를 랜덤하게 선택해서 리턴해줌
+import random
+
+# 1 ~ 999 중, 100 개의 숫자 랜덤 선택
+bst_num = set() # 중복 제거를 위해 사용
+while len(bst_num) != 100 :
+    bst_num.add(random.randint(0, 999))
+
+# 선택된 100개의 숫자를 이진 탐색 트리에 입력, 임의로 루트노드는 500을 넣기로 함(중앙값)
+head = Node(500)
+binary_tree = NodeMgmt(head)
+for num in bst_num :
+    binary_tree.insert(num)
+
+for num in bst_num :
+    if binary_tree.search(num) == False :
+        print('search failed', num)
+
+# 입력한 100개의 숫자 중 10개의 숫자를 랜덤 선택
+delete_num = set()
+bst_num = list(bst_num)
+while len(delete_num) != 10 :
+    delete_num.add(bst_num[random.randint(0, 99)])
+
+# 선택한 10기의 숫자를 삭제(삭제 기능 확인)
+for del_num in delete_num :
+    if binary_tree.delete(del_num) == False :
+        print('delete failed', del_num)
+
+"""
+5. 이진 탐색 트리의 시간 복잡도와 단점
+5-1. 시간 복잡도 (탐색시)
+    - depth (트리의 높이)를 h라고 표기한다면, O(h)
+    - n개의 노드를 가진다면, h = log2n에 가까우므로, 시간 복잡도는 O(logn)
+
+5-2. 이진 탐색 트리
+    - 평균 시간 복잡도 : O(log n)
+    - 최악 시간 복잡도 : O(n)  >> 최악의 경우 = 링크드 리스트와 동일한 성능을 보여줌 (1(root node) - 2 - 3 - 4 - 5) 
+"""
