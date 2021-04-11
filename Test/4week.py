5ae2f620490fa20a844c928840c5fb2acf2fd596
ysm7663-jn <ysm7663@gmail.com>
2021-04-11 22:26:11 +0900

    4주 시험

--- Test/4week.py
+++ Test/4week.py
@@ -49,36 +49,60 @@ print(pq.get())
 """
 """
 2번.
-오름차순으로 정렬된 N개의 정수를 가진 List가 주어져있을 때, 해당 List에 존재하는 서로 다른 값이 몇 가지인지 알아내는 알고리즘을 구현하라. 알고리즘의 제약사항은 아래와 같다. (알고리즘은 1 <= N <= 10000에서 테스트된다.)
+오름차순으로 정렬된 N개의 정수를 가진 List가 주어져있을 때,
+해당 List에 존재하는 서로 다른 값이 몇 가지인지 알아내는 알고리즘을 구현하라. 
+알고리즘의 제약사항은 아래와 같다. (알고리즘은 1 <= N <= 10000에서 테스트된다.)
 
 추가 메모리 사용은 O(1)으로 제한된다. 따라서 set()와 dict() 등의 자료구조를 사용할 수 없다.
 알고리즘의 시간복잡도는 O(N) 이하로 제한된다.
 """
-"""
+
 def countUniques(a):
-    pass
- 
+
+    count = 1
+
+    for i in range(len(a) - 1) :
+        for j in range(i+1, len(a)):
+            if a[i] != a[j] :
+                count += 1
+                break
+            else :
+                break
+    return count
+
 # Test code
 print(countUniques([-1, 1, 1, 1, 1, 4, 4, 4, 4, 10, 14, 14])) # 5
+print('------')
 print(countUniques([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])) # 2
-"""
+print('------')
+print(countUniques([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3])) # 4
+
+
+print('------')
 
 """
 3번
-N개의 문자열로 이루어진 List에서 전체 문자열이 앞 n개 문자열이 같다고 할때, 가장 큰 n을 출력하는 알고리즘을 구현하라. (즉, 주어진 모든 문자열의 앞의 몇개의 문자가 일치하는지 출력하라)
+N개의 문자열로 이루어진 List에서 전체 문자열이 앞 n개 문자열이 같다고 할때, 
+가장 큰 n을 출력하는 알고리즘을 구현하라. 
+(즉, 주어진 모든 문자열의 앞의 몇개의 문자가 일치하는지 출력하라)
 """
-"""
+
 def solution(a):
+    for i in a :
+        for j in a :
+            print(j, end=' ')        
+        print()
     return 0
- 
 # Test code
 print(solution(['abcd', 'abce', 'abchg', 'abcfwqw', 'abcdfg'])) # 3
-print(solution(['abcd', 'gbce', 'abchg', 'abcfwqw', 'abcdfg'])) # 0
-"""
+# print(solution(['abcd', 'gbce', 'abchg', 'abcfwqw', 'abcdfg'])) # 0
+print('------')
 
 """
 4번
-자연수 중, 각 자리수를 제곱한 것을 더하는 과정을 반복했을 때 1으로 끝나는 수를 '행복한 수'라고 한다. '행복한 수'가 아닌 경우 이 과정이 1에 도달하지 못하고 같은 수열이 반복되는 무한 루프에 빠지게 된다. 자연수를 입력받았을 때 '행복한 수'인지 판별하는 알고리즘을 작성하라.
+자연수 중, 각 자리수를 제곱한 것을 더하는 과정을 반복했을 때 1으로 끝나는 수를 '행복한 수'라고 한다.
+'행복한 수'가 아닌 경우 이 과정이 1에 도달하지 못하고 같은 수열이 반복되는 무한 루프에 빠지게 된다. 
+자연수를 입력받았을 때 '행복한 수'인지 판별하는 알고리즘을 작성하라.
 
 '행복한 수'를 찾는 과정의 예
 
@@ -88,11 +112,43 @@ print(solution(['abcd', 'gbce', 'abchg', 'abcfwqw', 'abcdfg'])) # 0
   6^2 + 8^2 = 100
   1^2 + 0^2 + 0^2 = 1 --> True
 """
-"""
+
 def solution(n):
-    return True
+
+    check = False
+
+    sum = 0
+    
+    while True :
+        
+        if n // 100 > 0 :
+            baek = n // 100
+            ship = n % 100
+            ill = n % 10
+            time_baek = pow(baek, 2)
+            time_ship = pow(ship, 2)
+            time_ill = pow(ill, 2)
+            print(time_baek, time_ship, time_ill)
+            sum = time_baek + time_ship + time_ill
+            print(sum)
+            
+        else :    
+            ship = n // 10
+            ill = n % 10
+            time_ship = pow(ship, 2)
+            time_ill = pow(ill, 2)
+            print(time_ship, time_ill)
+            sum = time_ship + time_ill
+            print(sum)
+            
+        if sum == 1:
+            check = True
+            return check
+        else :
+            n = sum
+    return check
  
 # Test code
 print(solution(19)) # True
-print(solution(61)) # False
-"""
+# print(solution(61)) # False
+
