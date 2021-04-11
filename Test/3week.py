6ea969a2694122773adbffb5ccfd4a51352e557f
ysm7663-jn <ysm7663@gmail.com>
2021-04-11 22:26:07 +0900

    3주 시험

--- Test/3week.py
+++ Test/3week.py
@@ -56,7 +56,7 @@ class LinkedQueue:
             return self.front.data
  
 
-"""
+
 # 2번 
 
 class Stack:
@@ -108,7 +108,7 @@ class Calculator:
 calc = Calculator()
 print(calc.calculate('4 6 * 2 / 2 +'))
 print(calc.calculate('2 5 + 3 * 6 - 5 *'))
-"""
+
 
 # 3번
 """
@@ -121,7 +121,7 @@ Node에 있는 data를 출력한다.
 Node에 left child가 있으면, left child node에 접근한다.
 Node에 right child가 있으면, right child node에 접근한다.
 root node에서 순회를 시작할 경우, 재귀적 동작으로 인해 모든 node의 data를 출력할 수 있다.
-
+"""
 class Node:
     def __init__(self, data, left=None, right=None):
         self.data = data
@@ -146,14 +146,13 @@ class Tree:
 root = Node(5, Node(2, Node(7, Node(4), Node(1)), Node(3)), Node(9, Node(6), Node(10)))
 tree = Tree(root)
 tree.preorder()
-"""
+
 
 # 4번
 """
 HashTable 클래스는 문자열을 key로 입력받는 해쉬 테이블 자료구조를 구현한 것이다. HashTable 클래스는 단순한 해쉬 함수로 인해, 해쉬 충돌이 빈번히 발생한다. 이 단점을 개선하기 위해, Chaining 기법으로 ChainedHashTable을 구현하고자 한다.
 
 HashTable을 상속하여 해쉬 충돌이 발생해도 정상적으로 동작하는 ChainedHashTable을 완성하시오.
-"""
 """
 def hash_func(key):
     return ord(key[0]) % 10
@@ -224,4 +223,3 @@ print(ht.get('hello'), end=' ')
 print(ht.get('hello2'), end=' ')
 print(ht.get('hello3'), end=' ')
 print(ht.get('hello4'), end=' ')
-"""
