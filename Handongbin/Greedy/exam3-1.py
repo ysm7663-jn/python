b7e8da66cacd79e6b1e00aa32a712e96b3a3329f
ysm7663-jn <ysm7663@gmail.com>
2021-04-12 16:01:05 +0900

    그리드 - 문자열 뒤집기

--- Handongbin/Greedy/exam3-1.py
+++ Handongbin/Greedy/exam3-1.py
@@ -8,11 +8,10 @@
 - 일반적인 상황에서 그리디 알고리즘은 최적의 해를 보장할 수 없을 때가 많다
 - 코테에서의 대부분의 그리디 문제는 탐욕법으로 얻은 해가 최적의 해가 되는 상황에서, 이를 추론할 수 있어야 풀리도록 출제된다.
 """
-"""
 # 3-1 거스름돈
 # 가장 큰 화폐 단위부터 거슬러 준다
 # 화폐의 종류가 K일 때, 소스코드의 시간 복잡도는 O(K)
-
+"""
 N = 1260
 cnt = 0
 arr = [500, 100, 50, 10]
@@ -21,10 +20,10 @@ for coin in arr :
     cnt += N // coin
     N %= coin
 print(cnt)
-
-
+"""
 # 3-2 1이 될 때까지
 # 내 풀이
+"""
 N, K = map(int, input().split())
 
 cnt = 0
@@ -38,9 +37,10 @@ while N != 1 :
         N -= 1
         cnt += 1
 print(cnt)
-
+"""
 # 수업 풀이
 # 실행 시간이 (O(logN)) 으로 줄어든다
+"""
 a, b = map(int, input().split())
 
 result = 0
@@ -62,9 +62,9 @@ while True :
 # 마지막으로 남은 수에 대하여 1씩 빼기
 result += (a - 1)
 print(result)
-
-
+"""
 # 3-3 곱하기 혹은 더하기
+"""
 arr = input()
 
 result = int(arr[0])
@@ -78,8 +78,9 @@ for i in range(1, len(arr)) :
         result *= num
 
 print(result)
-
+"""
 # 3-4 모험가 길드
+"""
 n = int(input())
 data = list(map(int, input().split()))
 data.sort()
@@ -93,8 +94,9 @@ for i in data :
         result += 1
         count = 0
 print(result)
-
+"""
 # 3-5 큰 수의 법칙
+"""
 n, m, k = map(int, input().split())
 
 num = list(map(int, input().split()))
@@ -116,10 +118,49 @@ while True :
         break    
     sum += second
     m -= 1
-
 print(sum)
 """
 # 3-6 숫자 카드 게임
+"""
+n, m = map(int,input().split())
 
+result = 0
+for i in range(n) :
+    data = list(map(int, input().split()))
+    min_value = min(data)
+    result = max(result, min_value)
+print(result)
+"""
+"""
+내가 입력한 값
+min_list = []
+for i in range(n) :
+    for j in range(m) :
+        min_list.append(min(data[i]))
+result = 0
 
+for i in min_list :
+    result = max(min_list)
+print(result)
+"""
 
+# 3-7 문자열 뒤집기
+"""
+data = input()
+
+count0 = 0
+count1 = 0
+
+if data[0] == '1' :
+    count1 += 1
+else :
+    count0 += 1
+
+for i in range(len(data)-1) :
+    if data[i] != data[i+1] :
+        if data[i+1] == '1' :
+            count0 += 1
+        else :
+            count1 += 1
+print(min(count0, count1))
+"""
