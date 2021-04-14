fdc93d2325fd1e642211a896c5ec15ddb6b038bc
ysm7663-jn <ysm7663@gmail.com>
2021-04-14 12:56:47 +0900

    2021-04-14 N9095 1,2,3 더하기

---
+++ Algorithm/etc/N9090.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+def func(data) :
+    if data == 1:
+        return 1
+
+    elif data == 2:
+        return 2
+
+    elif data == 3:
+        return 4
+
+    elif data > 3:
+        return func(data - 1) + func(data - 2) + func(data - 3)
+
+    else :
+        return
+
+for i in range(N) :
+    print(func(int(input())))
