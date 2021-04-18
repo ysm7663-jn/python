8b0aede8ab08bf7e52a2682d865223e4c9d30755
ysm7663-jn <ysm7663@gmail.com>
2021-04-18 17:52:12 +0900

    test

--- test.py
+++ test.py
@@ -1,26 +1,23 @@
-def func(data, target):
-    print(data)
-    for i in range(len(data[0])):
-        if len(data) == 1 and target == data[0][i]:
-            return True
+from math import gcd
+def gcdString(A, B):
+    
+    if A+B != B+A:
+        return ''
 
-        if len(data) == 1 and target != data[0][i]:
-            return False
+    len_A = len(A)
+    len_B = len(B)
+    result_gcd = (gcd(len_A, len_B))
 
-        if len(data) == 0:
-            return False
+    
+    if len_A < len_B:
+        return A[0 : result_gcd]
+    else:
+        return B[0 : result_gcd]
 
-    mid = len(data) // 2
 
-    for i in range(len(data[mid])):
-        if target == data[mid][i]:
-            return True
-        else:
-            if target > data[mid][0]:
-                return func(data[mid:], target)
-            else:
-                return func(data[:mid], target)
-
-data = [[1, 3, 5, 7],[10, 11, 16, 20],[23, 30, 34, 50]]
-target = 30
-print(func(data, target))
+print(gcdString('ababcde','ababcde'))
+print(gcdString('ababababab','abab'))
+print(gcdString('abababab','abab'))
+print(gcdString('fast','campus'))
+print(gcdString('abc','bcd'))
+print(gcdString('abc','abcabc'))
