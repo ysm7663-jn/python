5e233fbdd6a745b23fdca0fa5a3aadc89820a725
ysm7663-jn <ysm7663@gmail.com>
2021-04-19 09:46:39 +0900

    2021-04-19 section20 BFS

--- Algorithm_study/section20.py
+++ Algorithm_study/section20.py
@@ -20,3 +20,22 @@ graph['I'] = ['C', 'J']
 graph['J'] = ['I']
 
 print(graph) # {'A': ['B', 'C'], 'B': ['A', 'D'], 'C': ['A', 'G', 'H', 'I'], 'D': ['B', 'E', 'F'], 'E': ['D'], 'F': ['D'], 'G': ['C'], 'H': ['C'], 'I': ['C', 'J'], 'J': ['I']}
+
+def bfs(graph, start_node):
+    visited = list()
+    need_visit = list()
+
+    need_visit.append(start_node)
+
+    while need_visit:
+        node = need_visit.pop(0)
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+        
+    return visited
+
+print(bfs(graph,'A')) # ['A', 'B', 'C', 'D', 'G', 'H', 'I', 'E', 'F', 'J']
+"""
+시간 복잡도 = O(V + E)
+노드 수 : V / 간선 수 : E
