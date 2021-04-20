0e500bee8ce69dded5690ece8f6bbb951a6dfbbb
ysm7663-jn <ysm7663@gmail.com>
2021-04-20 23:54:44 +0900

    2021-04-20 N13305 주유소

--- Algorithm_BaekJoon/level16/N13305.py
+++ Algorithm_BaekJoon/level16/N13305.py
@@ -3,27 +3,12 @@ N = int(input())
 length = list(map(int,input().split()))
 cost = list(map(int,input().split()))
 
-total_length = sum(length)
-sum = 0
+total_cost = 0
+min_cost = cost[0]
 
-min_cost = 0
+for i in range(len(cost)-1):
+    if min_cost > cost[i]:
+        min_cost = cost[i]
+    total_cost += min_cost * length[i]
 
-for i in range(N-1) :
-
-    for j in range(len(cost)-1) :
-        if cost[j] < cost[j+1] :
-            min_cost = cost[j]
-
-    print('min_cost', min_cost)
-
-    if cost[i] == min_cost :
-        sum += total_length * cost[i]
-        print('if',sum)
-        break
-    else :
-        sum += cost[i] * length[i]
-        total_length -= length[i]
-        print('sum',sum)
-        print('total',total_length)
-print(sum)
-
+print(total_cost)
