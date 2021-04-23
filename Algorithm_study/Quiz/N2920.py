ea63837ce05b5bdea02b01ef2fa28f9b6da60c6a
ysm7663-jn <ysm7663@gmail.com>
2021-04-23 15:11:38 +0900

    N2920 음계

--- Algorithm_study/Quiz/N2920.py
+++ Algorithm_study/Quiz/N2920.py
@@ -1,5 +1,6 @@
-data_list = list(map(int, input().split()))
-
+data_list = list(map(int, input().split(' ')))
+"""
+내 풀이
 a = sorted(data_list)
 b = sorted(data_list, reverse=True)
 
@@ -11,6 +12,23 @@ for i in data_list:
 if a == data_sort:
     print('ascending')
 elif b == data_sort:
+    print('descending')
+else:
+    print('mixed')
+"""
+
+ascending = True
+descending = True
+
+for i in range(1, 8):
+    if data_list[i] > data_list[i-1]:
+        descending = False
+    elif data_list[i] < data_list[i-1]:
+        ascending = False
+
+if ascending:
+    print('ascending')
+elif descending:
     print('descending')
 else:
     print('mixed')
