953dba20bbeba679ed58e40f4527986f11f8191d
ysm7663-jn <ysm7663@gmail.com>
2021-04-28 17:10:36 +0900

    test

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-data_list = [1, 1, 3, 4, 1]
+k = int(input())
 
-f_data = data_list[4] 
-print(f_data)
-print(data_list.index(f_data))
+def hanoi(k, a, b, c):
+    if k == 1:
+        print(a, c)
+    else:
+        hanoi(k - 1, a, c, b)
+        print(a, c)
+        hanoi(k - 1, b, a, c)
+
+sum = 1
+for i in range(k-1):
+    sum = sum * 2 + 1
+print(sum)
+
+hanoi(k, 1, 2, 3)
