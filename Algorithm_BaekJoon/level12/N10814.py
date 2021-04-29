dd1b5251ae64b5928b6f843437387b928076c521
ysm7663-jn <ysm7663@gmail.com>
2021-04-29 20:27:40 +0900

    N10814

--- Algorithm_BaekJoon/level12/N10814.py
+++ Algorithm_BaekJoon/level12/N10814.py
@@ -6,8 +6,21 @@ for _ in range(N):
     age = int(age)
     data_list.append((age, name))
 
-print(data_list)
 data_list.sort(key = lambda x : (x[0]))
 
 for i in data_list:
     print(i[0], i[1])
+
+"""
+n = int(input())
+
+array = []
+
+for _ in range(n):
+    input_data = input().split(' ')
+    array.append((int(input_data[0]), input_data[1]))
+
+array = sorted(array, key = lambda x: x[0])
+for i in range array:
+    print(i[0], i[1])
+"""
