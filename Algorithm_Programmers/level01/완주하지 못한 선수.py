3873d7e3a1db38aa0c2bbbf47abd1bdf77faab5c
ysm7663-jn <ysm7663@gmail.com>
2021-05-04 17:51:59 +0900

    완주하지 못한 선수

--- Algorithm_Programmers/level01/완주하지 못한 선수.py
+++ Algorithm_Programmers/level01/완주하지 못한 선수.py
@@ -8,16 +8,28 @@ def solution(participant, completion):
         if p != c:
             return p    
     return participant.pop()
+
+"""
+zip(a, b)
+
+numbers = [1, 2, 3]
+letters = ["a", "b", "c"]
+
+for pair in zip(numbers, letters):
+    print(pair)
+
+(1, "a")
+(2, "b")
+(3, "c")
+"""
+
 """
 import collections
-
 
 def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
     return list(answer.keys())[0]
 """
-
-
 
 participant = ['leo', 'kiki', 'eden']
 completion = ['eden', 'kiki']
