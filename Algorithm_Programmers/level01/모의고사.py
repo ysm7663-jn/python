1a58255d7279b29751ec670f71e09e55916df2f8
ysm7663-jn <ysm7663@gmail.com>
2021-05-06 09:24:29 +0900

    모의고사

---
+++ Algorithm_Programmers/level01/모의고사.py
@@ -0,0 +1,26 @@
+def solution(answers):
+    answer = [0, 0, 0]
+    winner = []
+
+    s1 = [1, 2, 3, 4, 5]
+    s2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    for i in range(len(answers)):
+        if answers[i] == s1[(i%5)]:
+            answer[0] += 1
+
+        if answers[i] == s2[(i%8)]:
+            answer[1] += 1
+
+        if answers[i] == s3[(i%10)]:
+            answer[2] += 1    
+
+    for i in range(3):
+        if answer[i] == max(answer):
+            winner.append(i+1)
+    
+    return winner
+
+print(solution([1, 2, 3, 4, 5]))
+print(solution([1, 3, 2, 4, 2]))
