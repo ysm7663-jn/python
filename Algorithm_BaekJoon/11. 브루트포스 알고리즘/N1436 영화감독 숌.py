63ca5177a0ee684828aba18d1241c6d18fbc725e
ysm7663-jn <ysm7663@gmail.com>
2021-05-11 14:14:21 +0900

    N1436 영화감독 숌

---
+++ Algorithm_BaekJoon/11. 브루트포스 알고리즘/N1436 영화감독 숌.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+answer = 666
+cnt = 0
+
+while True:
+    if '666' in str(answer):
+        cnt += 1
+    if cnt == N:
+        print(answer)
+        break
+    answer += 1
