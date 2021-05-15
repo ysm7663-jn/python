fbcf9d728c2b515fcdad85d952e8297eae8ca206
ysm7663-jn <ysm7663@gmail.com>
2021-05-15 19:05:19 +0900

    4. 힙(heapq)

--- Algorithm_Handongbin/문법/4. 힙(heapq).py
+++ Algorithm_Handongbin/문법/4. 힙(heapq).py
@@ -19,6 +19,5 @@ def heapsort(iterable):
         result.append(heapq.heappop(h))
     return result
 
-
 result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
 print(result) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
