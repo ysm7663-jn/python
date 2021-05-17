b192348cb3c5fb7a678ba0a6a592d7bf3c599f7b
ysm7663-jn <ysm7663@gmail.com>
2021-05-17 10:51:54 +0900

    4. 힙

--- Algorithm_Handongbin/문법/4. 힙(heapq).py
+++ Algorithm_Handongbin/문법/4. 힙(heapq).py
@@ -21,3 +21,25 @@ def heapsort(iterable):
 
 result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
 print(result) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# 파이썬에서는 최대힙을 제공하지 않는다.
+# 따라서 heapq 라이브러리를 이용해서 최대힙을 구현할 때는 원소의 부호를 임시로 변경하는 방식 활용
+
+import heapq
+
+def heapsort2(iterable):
+    h2 = []
+    result2 = []
+
+    # 모든 원소를 차례대로 힙에 삽입
+    for value in iterable:
+        heapq.heappush(h2, -value)
+    
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for i in range(len(h2)):
+        result2.append(-heapq.heappop(h2))
+
+    return result2
+
+result2 = heapsort2([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
+print(result2) # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
