d80548e5cb42d1c756ebcd32cddfed93cec6d573
ysm7663-jn <ysm7663@gmail.com>
2021-05-17 10:52:08 +0900

    7. math

---
+++ Algorithm_Handongbin/문법/7. math.py
@@ -0,0 +1,23 @@
+# 수학적인 기능을 포함하고 있는 라이브러리
+
+# 1. 팩토리얼
+import math
+
+print(math.factorial(5))  # 5! / 120
+
+# 2. 제곱근
+import math
+print(math.sqrt(7)) # 7의 제곱근 / 2.6457513110645907
+
+# 3. 최대 공약수
+import math
+print(math.gcd(21, 14)) # 7
+
+# 4. pi
+import math
+print(math.pi) # 3.141592653589793 
+
+# 5. e
+import math
+print(math.e) # 2.718281828459045 
+
