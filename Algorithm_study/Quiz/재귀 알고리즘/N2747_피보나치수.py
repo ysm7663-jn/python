fe3a9c5b8f4e3c6802dd55d4f5409ccefa8a0da9
ysm7663-jn <ysm7663@gmail.com>
2021-05-19 16:46:03 +0900

    N2747 피보나치 수

---
+++ Algorithm_study/Quiz/재귀 알고리즘/N2747_피보나치수.py
@@ -0,0 +1,21 @@
+# (재귀함수 사용x)
+import sys
+
+n = int(sys.stdin.readline())
+
+sum = [0 for i in range(n+1)]
+
+sum[1] = 1
+
+for i in range(2, len(sum)):
+    sum[i] = sum[i-1] + sum[i-2]
+
+print(sum[-1])
+
+"""
+a, b = 0, 1
+while n > 0:
+    a, b = b, a + b
+    n -= 1
+print(a)
+"""
