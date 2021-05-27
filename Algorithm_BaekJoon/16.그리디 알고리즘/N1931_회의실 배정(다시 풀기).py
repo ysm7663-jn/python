ae9ca9a50f9c6d79b24e61b6834a4e5a75df1aa3
ysm7663-jn <ysm7663@gmail.com>
2021-05-27 14:36:02 +0900

    N1931_회의실 배정(다시 풀기)

--- Algorithm_BaekJoon/16.그리디 알고리즘/N1931_회의실 배정(다시 풀기).py
+++ Algorithm_BaekJoon/16.그리디 알고리즘/N1931_회의실 배정(다시 풀기).py
@@ -1,12 +1,11 @@
-import sys
-
-n = int(sys.stdin.readline())
-
-data = []
 
 """
 # 내 풀이 >> 시간 초과
 # 접근 방식은 맞았지만 lambda를 활용한 각각의 정렬을 따로 해줘야할 듯
+n = int(input())
+
+data = []
+
 for i in range(n) :
     data.append(list(map(int, sys.stdin.readline().split())))
 # [[1, 4], [3, 5], [0, 6], [5, 7], [3, 8], [5, 9], [6, 10], [8, 11], [8, 12], [2, 13], [12, 14]]
@@ -33,22 +32,17 @@ while True:
 
 print(max(result))
 """
+n = int(input())
+s = []
 for i in range(n):
     first, second = map(int, input().split())
-    data.append([first, second])
-
-s = sorted(data, key = lambda x : x[1]) # 종료 시간에 대한 오름차순 정렬
-print(s) # [[1, 4], [3, 5], [0, 6], [5, 7], [3, 8], [5, 9], [6, 10], [8, 11], [8, 12], [2, 13], [12, 14]]
-# s = sorted(data, key = lambda x : x[0]) # 시작 시간에 대한 오름차순 정렬 
-# print(s) [[0, 6], [1, 4], [2, 13], [3, 5], [3, 8], [5, 7], [5, 9], [6, 10], [8, 11], [8, 12], [12, 14]]
-# s = sorted(data, key = lambda x : x[1]) # 종료 시간에 대한 오름차순 정렬
-# print(s) [[1, 4], [3, 5], [0, 6], [5, 7], [3, 8], [5, 9], [6, 10], [8, 11], [8, 12], [2, 13], [12, 14]]
-
+    s.append([first, second])
+s = sorted(s, key=lambda a: a[0])
+s = sorted(s, key=lambda a: a[1])
 last = 0
 cnt = 0
-
 for i, j in s:
     if i >= last:
-       cnt += 1
-       last = j
-print(cnt) 
+        cnt += 1
+        last = j
+print(cnt)
